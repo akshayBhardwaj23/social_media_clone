a2b8877e4f1a3d7328b0d64cf96fa3eff1681db6
Akshay Bhardwaj <akshaybhardwaj96.ab@gmail.com>
2024-02-14 01:40:53 +0530

    Setting Up Basic Leftsidebar

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -1,7 +1,32 @@
 import React from "react";
+import { sidebarLinks } from "@/constants";
+import Link from "next/link";
+import Image from "next/image";
 
 const LeftSidebar = () => {
-  return <h1>LeftSidebar</h1>;
+  return (
+    <section className="custom-scrollbar leftsidebar">
+      <div className="flex w-full flex-1 flex-col gap-6 px-6">
+        {sidebarLinks.map((link) => {
+          return (
+            <Link
+              href={link.route}
+              key={link.label}
+              className="leftsidebar_link"
+            >
+              <Image
+                src={link.imgURL}
+                alt={link.label}
+                width={24}
+                height={24}
+              />
+              <p className="text-light-1 max-lg:hidden">{link.label}</p>
+            </Link>
+          );
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default LeftSidebar;
